2d185535a73cb1c92f690eb1b5a7db40f4f79c23
Subhakanta Roul <papu.roul99@gmail.com>
2026-03-07 00:36:32 +0530

    client: react vite project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+import Layout from './Layout';
+
+function App() {
+    return (
+      <Layout>
+      </Layout>
+    )
+}
+
+export default App;
---
+++ client/src/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductForm from './components/form/ProductForm'
+
+function Layout() {
+  return (
+    <div>
+        <ProductForm/>
+    </div>
+  )
+}
+
+export default Layout
