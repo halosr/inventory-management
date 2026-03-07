8428d51b42e81cbf0759f34943ce7e39200721bf
Subhakanta Roul <papu.roul99@gmail.com>
2026-03-08 00:21:14 +0530

    client: app entry point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import './App.css';
-import Layout from './Layout';
+import Layout from '@/Layout';
+import ProductForm from '@/components/productform/ProductForm';
+import Summary from '@/components/summary/Summary';
+import ProductTable from '@/components/producttable/ProductTable';
 
 function App() {
     return (
-      <Layout>
-      </Layout>
-    )
+        <>
+            <Layout>
+                <Summary/>
+                <ProductForm />
+                <ProductTable/>
+            </Layout>
+        </>
+    );
 }
 
 export default App;
