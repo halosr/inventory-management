5f8db36306e4c06ab3fa9ca13f32b2727468c196
Subhakanta Roul <papu.roul99@gmail.com>
2026-03-15 00:04:59 +0530

    client: api endpooint configs

---
+++ client/src/api/products.api.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import type { Product } from '@/types/product';
+
+export async function getAllProductsApi() {
+    const data = await axios.get(`http://localhost:8000/products`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return data;
+}
+export async function getProductApi(id: number) {
+    const data = await axios.get(`http://localhost:8000/products/${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    return data;
+}
+
+export async function addProductApi(product: Product) {
+    const data = await axios.post(`http://localhost:8000/products`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: product,
+    });
+    return data;
+}
+
+export async function updateProductApi(id: number, product: Product) {
+    const data = await axios.put(`http://localhost:8000/products/${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: product,
+    });
+    return data;
+}
+
+export async function deleteProductApi(id: number) {
+    const data = await axios.delete(`http://localhost:8000/products/${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    return data;
+}
