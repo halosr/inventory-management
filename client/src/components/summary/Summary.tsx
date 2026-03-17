439eb8fd65f5fe02d1dc410ab03c973acc6daea4
Subhakanta Roul <papu.roul99@gmail.com>
2026-03-18 01:31:32 +0530

    client: update summary component to show the count and referesh the page

--- client/src/components/summary/Summary.tsx
+++ client/src/components/summary/Summary.tsx
@@ -1,10 +1,22 @@
-import { Box, Button, Paper } from '@mui/material';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { clearForm, getProducts } from '@/store/product.slice';
+import { Box, Button, Paper, Typography } from '@mui/material';
 
 function Summary() {
+    const { productList } = useAppSelector((state) => state?.products);
+    const dispatch = useAppDispatch();
+    const handleRefresh = () => {
+        dispatch(getProducts());
+        dispatch(clearForm());
+    };
     return (
         <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '20px' }}>
-            <Paper>Total: 5</Paper>
-            <Button variant="contained" sx={{ gridColumn: '4' }}>
+            <Paper sx={{ textAlign: 'center' }}>
+                <Typography variant="h4">
+                    Total: <span>{productList.length}</span>
+                </Typography>
+            </Paper>
+            <Button variant="contained" sx={{ gridColumn: '4' }} onClick={() => handleRefresh()}>
                 Refresh
             </Button>
         </Box>
